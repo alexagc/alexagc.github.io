6c2cf635c3b7b5a700fd323b4c695ed370c66f23
alexagc <alexcanal@gmail.com>
2017-09-07 09:52:07 +0200

    change how app init

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,4 +1,9 @@
 (function () {
+  angular.module('app', ['ngMaterial'])
+    .run(() => {
+      console.log('APP initialize')
+    });
+
   window.onload = function () {
     angular.bootstrap(document.body, ['app']);
   };
